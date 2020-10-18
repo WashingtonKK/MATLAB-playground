1410db462ed9a188db31406c3286bd699e08e3ac
WashingtonKK <washingtonkigan@gmail.com>
2020-10-18 16:38:04 +0300

    Scripts

---
+++ Learning/scripts/mathematics.m
@@ -0,0 +1,24 @@
+y = idivide (int32(5), int32(3));
+x = idivide (int32(5), int32(3), 'ceil');
+a = mod (5,3);
+plus (5, 6);
+minus (5,1);
+%Unary function, negates
+uminus(-2);
+%Unary function, negates
+uminus(2);
+%Gives the remainder
+rem (5,2);
+
+%Unary function
+uplus(-4);
+
+%Assignment
+A = 5;
+B = 3;
+
+%Checking for equality
+A == B;
+
+%Not equal to
+A ~= B;
---
+++ Learning/scripts/vectors_and_matrices.m
@@ -0,0 +1,55 @@
+%%VECTORS AND MATRICES
+
+%Inputing a list of numbers
+%A 1x4 list
+b = [5, 6, 7, 8];
+%To have a vertical list we use semicolons
+e = [1;5;3];
+%A multidimensional list
+%A matrix
+f = [1,2,3;
+    4,5,6;
+    7,8,9];
+%Instead of indicating a list of numbers
+%We use range
+%This is done using a semi-colon
+%Default step is 1
+g = [5:10];
+%To specify the number of values
+%We can use a more convenient function
+%Linspace
+%The third argument is the number of values
+h = linspace(4, 15, 20);
+%A similar result can be gotten using the colon
+j = [4 : 0.5 : 16];
+%Loading the saved variable B
+load("Learning\B.mat", "B")
+
+%Exporting variable h as an xls file
+xlswrite("Learning\h.xls",h)
+
+%Exporting as csv
+csvwrite("Learning\j.csv", j)
+
+%Exporting as a single file with both variables
+csvwrite("Learning\combination.csv", h,j, g)
+
+%Transposing the vector to form a column vector
+k = [1: 0.4 : 10]';
+%Transposing matrices
+trans = [1,2,3;
+    4,5,6;
+    7,8,9;
+    10,11,12];
+
+%Transposing is done using an apostrophe
+trans = trans';
+trans;
+%Addition and subtracton
+add1 = [1,2,3;
+    4,5,6];
+add2= [2,3,4;
+    5,6,7];
+sum = add1 + add2;
+difference = add2 - add1;
+%Multiplication and division
