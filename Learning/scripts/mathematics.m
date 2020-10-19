17562fe1274047e0ebbbfc6f66ca2ad5372b8fe4
WashingtonKK <washingtonkigan@gmail.com>
2020-10-19 15:00:47 +0300

    Modified scriptss

--- Learning/scripts/mathematics.m
+++ Learning/scripts/mathematics.m
@@ -1,24 +1,37 @@
-y = idivide (int32(5), int32(3));
-x = idivide (int32(5), int32(3), 'ceil');
-a = mod (5,3);
-plus (5, 6);
-minus (5,1);
+y = idivide (int32(5), int32(3))
+x = idivide (int32(5), int32(3), 'ceil')
+a = mod (5,3)
+plus (5, 6)
+minus (5,1)
 %Unary function, negates
-uminus(-2);
+uminus(-2)
 %Unary function, negates
-uminus(2);
+uminus(2)
 %Gives the remainder
-rem (5,2);
+rem (5,2)
 
 %Unary function
-uplus(-4);
+uplus(-4)
 
 %Assignment
 A = 5;
 B = 3;
 
 %Checking for equality
-A == B;
+A == B
 
 %Not equal to
-A ~= B;
+A ~= B
+
+%%Complex numbers
+%The result contains imaginary numbers
+[1,2; 3,4]^(0.5)
+
+
+%%Exponents
+
+ah = expm([1,2; 3,4])
+
+%For element by element exponents we use
+ai = exp([1,2; 5,6])
+
