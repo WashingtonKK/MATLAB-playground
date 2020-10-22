6e8436d5fecdec70b4b89f19f0e60fa034ad82e4
WashingtonKK <washingtonkigan@gmail.com>
2020-10-22 22:11:14 +0300

    An implementation of freudenstein's equation

---
+++ Projects/labwork/freudenstein.m
@@ -0,0 +1,19 @@
+function [solution] = freudenstein(inputAngle,outputAngle)
+%%Implementing the freudensteins equation
+%Receives three values for the input angle and three values for the output
+%   angle and solves for K1, K2 and K3
+%%
+%Creating a 3x3 matrix with all zeros
+%eq = zeros(3,3);
+
+eq ( :, :) = [ cosd(outputAngle), -cosd(inputAngle), [1;1;1] ];
+sol = cosd(inputAngle - outputAngle);
+%%
+%Finding the determinant of eq
+%Solving the simultaneous equation for K1, K2, K3
+solution = linsolve(eq, sol);
+
+
+
+end
+
