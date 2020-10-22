a4362d1d76392769330ecfc0a9f2b976e011fcbd
WashingtonKK <washingtonkigan@gmail.com>
2020-10-22 22:09:45 +0300

    Modified

--- Projects/labwork/chebyshev.m
+++ Projects/labwork/chebyshev.m
@@ -1,4 +1,4 @@
-function [precisionPoints1] = chebyshev(theta1,theta2, n)
+function [precisionPoints] = chebyshev(theta1,theta2, n)
 %An implementation of the chebyshev spacing equation
 %Input parameters: (theta1 <= theta <= theta2) for j number of precision
 %points
@@ -9,22 +9,13 @@ function [precisionPoints1] = chebyshev(theta1,theta2, n)
 % theta = 0.5(theta1 + theta2) + 0.5(theta2 - theta1)*cos((pi*(2*j -1)/2n)
 
 precisionPoints = zeros(n,1);
-precisionPoints1 = zeros(n,1);
-li = n;
 
     for index = 1:n
-        a = n;
-        x = 0.5*(theta1 + theta2)+ 0.5*(theta2 - theta1)*cos((pi*(2*a - 1)/(2*li)));
-        precisionPoints (n, 1) = x;
-       % precisionPoints (n, 1) = 0.5*(theta1 + theta2)+ 0.5*(theta2 - theta1)*cos((pi*(2*n - 1)/(2*li)));
+%         x = 0.5*(theta1 + theta2)+ 0.5*(theta2 - theta1)*cos((pi*(2*a - 1)/(2*li)));
+%         precisionPoints (n, 1) = x;
+        precisionPoints (index, 1) = 0.5*(theta1 + theta2)+ 0.5*(theta2 - theta1)*cos((pi*(2*index - 1)/(2*n)));
 
     end
     
-    i = 1;
-    while ~(i < n)
-        x = 0.5*(theta1 + theta2)+ 0.5*(theta2 - theta1)*cos((pi*(2*i - 1)/(2*n)));
-        precisionPoints1 (i, 1) = x;
-        i = i+1;
-    end
 end
 
