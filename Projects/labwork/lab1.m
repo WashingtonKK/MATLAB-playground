0cceeac7d72e79b1ef89236e3993de1f5aa9eaa3
WashingtonKK <washingtonkigan@gmail.com>
2020-10-24 13:32:11 +0300

    Modified

--- Projects/labwork/lab1.m
+++ Projects/labwork/lab1.m
@@ -1,7 +1,6 @@
 %%Dynamics of Planar Mechanisms, lab work 1
-%Done by; 
-%%
-%%Gateru Felix Wanyoike   -- ENM221-
+%Done by;
+%Gateru Felix Wanyoike    -- ENM221-
 %Washington Kigani Kamadi -- ENM221-0063/2017
 %Michael Kimani Gitau     -- ENM221-
 %Brown                    -- ENM221-
@@ -13,13 +12,13 @@
 %It takes 3 arguments, the maximum and minimum value of the range and the
 %   number of precision points.
 
+%%
 %%Using chebyshev to find the precision points
 %The precision points are stored in a column vector called precisionPoints
 precisionPoints = chebyshev(15, 165, 3);
 
 input = precisionPoints;
 
-%%
 %The required function
 % theta4 = 65 + 0.43*theta2
 %The above is implemented as a function taking one argument for theta2 and
@@ -29,25 +28,28 @@ output = rel(input);
 
 %%
 %Solving for the values of K1, K2 and K3 using the freudenstein's equation
-K = freudenstein(input, output);
+K = (freudenstein(input, output));
 
 %%
 %Solving for the lengths of the links
 %a,b and c -- given that the length of d is fixed
-d = 0.410;
-a = abs ( d * ( K(1,1) ) );
-c = abs ( d * ( K(2,1) ) );
-b = sqrt( (a^2) + (c^2) + (d^2) - (2*a*c*(K(3,1)) ) );
- 
+fixed = 0.410;
+crank = (fixed/(K(1,1)));
+follower = (fixed/(K(2,1)));
+coupler = sqrt((crank^2)+(follower^2)+(fixed^2) - (2*crank*follower*(K(3,1))));
+
 %%
-%Solving for the transmission angles
+%%Solving for the transmission angles
 %Using the range of input angles from 30 degrees to 150degrees
-input = (30: 5: 150)';
+input = (0 : 5: 280)';
 
 %%
-%Computing the transmission angle
-transmission = transmissionAngle(a,d,c,d,input);
+%%Computing the transmission angle
+transmission = transmissionAngle(crank,coupler,follower,fixed,input);
 
 %%
-%Plotting the values of the transmission angle against the input angle
-plot( input, transmission )
+%%Plotting the values of the transmission angle against the input angle
+plot(input, transmission)
+title ('Transmission angle against input angle');
+xlabel('Input angle');
+ylabel('Transmission angle');
--- Projects/labwork/transmissionAngle.m
+++ Projects/labwork/transmissionAngle.m
@@ -1,10 +1,10 @@
-function [transmissionAngl] = transmissionAngle(a, b, c, d, input)
+function [Angl] = transmissionAngle(crank, coupler, follower, fixed, input)
     %%Using this function to compute the transmission angles based on a
     %  particular input angle
-    a2 = a^2;
-    b2 = b^2;
-    c2 = c^2;
-    d2 = d^2;
-    transmissionAngl = ( (b2 + c2 - a2 - d2 + 2*a*d*(cosd(input)) ) / (2*b*c) );
+    %     a2 = a^2;
+    %     b2 = b^2;
+    %     c2 = c^2;
+    %     d2 = d^2;
+    Angl = acosd( ((coupler^2) + (follower^2) - (crank^2) - (fixed^2) + (2*crank*fixed*(cosd(input))) ) / (2*coupler*follower) );
 end
 
