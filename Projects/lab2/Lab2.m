afb12a1dacbd863e5f12ae6112e685536a3f6256
WashingtonKK <washingtonkigan@gmail.com>
2020-11-12 11:46:44 +0300

    Second lab work added

---
+++ Projects/lab2/Lab2.m
@@ -0,0 +1,14 @@
+%%
+%The striking mechanism of a weaving machine consists of a four bar linkage. 
+%In a particular case, such a linkage is required to coordinate five positions of the rocker follower
+%with five positions of the input crank as shown in the Table 1. The length of the fixed
+%link is required to be 180mm.
+%%
+%Values of input and output angles are given below
+input_angle = [40; 45; 50; 55; 60]
+output_angle = [70; 76; 83; 91; 100] 
+
+%%
+%Evaluate K1, K2 and K3 using the least square method, and hence determine the
+%length of the other links
+
--- Projects/labwork/mike/matlabcode/README.md
+++
@@ -1,2 +0,0 @@
-# matlabcode
-Program to calculate link lengths, transmission angles and structural errors in a given four bar linkage
